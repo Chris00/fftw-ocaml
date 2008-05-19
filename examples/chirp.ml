0c4b1a9175cb03fd38e1018c3ddda23261fef6f7
Christophe TROESTLER <Christophe.Troestler@umh.ac.be>
2008-05-19 02:14:33 +0200

    Added the [filter] function to the chirp example and modified the graphics accordingly.

--- examples/chirp.ml
+++ examples/chirp.ml
@@ -1,13 +1,22 @@
 (* FFT analysis of a "chirp" signal.
 
-   Inspired from
-   http://ccrma.stanford.edu/~jos/sasp/Computational_Examples_Matlab.html *)
+   Inspired by
+   http://ccrma.stanford.edu/~jos/sasp/Computational_Examples_Matlab.html
+
+   Remark: The could could have been shortened by using Lacaml.  We
+   did not however to avoid this additional dependency.
+*)
 
 open Bigarray
 module FFT = Fftw3.D
-type vec = c_layout FFT.Array1.float_array
-
 module G = Gnuplot.Bigarray
+
+type vec = fortran_layout FFT.Array1.complex_array
+
+let pi = 4. *. atan 1.
+
+(** Chirp signal
+ ***********************************************************************)
 
 type shape = Linear | Quadratic | Logarithmic
 
@@ -39,36 +48,113 @@ let chirp ?f0 ?(t1=1.) ?(f1=100.) ?(phase=0.) = function
       and x = df**(1. /. t1) in
       (fun t -> cos(a *. x**t +. b *. t +. phase))
 
+(** Filter function (akin matlab one)
+ ***********************************************************************)
 
+let create kind n = FFT.Array1.create kind fortran_layout n
+
+let copy0 (x:vec) (y:vec) =
+  for i = 1 to Array1.dim x do y.{i} <- x.{i} done;
+  for i = Array1.dim x + 1 to Array1.dim y do y.{i} <- Complex.zero done
+
+let scal s (x:vec) =
+  for i = 1 to Array1.dim x do
+    let xi = x.{i} in
+    x.{i} <- { Complex.re = s *. xi.Complex.re; im = s *. xi.Complex.im }
+  done
+
+(** [filter b ?a x] returns [y] the data in vector [x] filtered with
+    the filter described by vectors [a] and [b].  The filter
+    implements of the standard difference equation:
+    {v
+    a(1)*y(n) = b(1)*x(n) + b(2)*x(n-1) + ... + b(nb+1)*x(n-nb)
+                          - a(2)*y(n-1) - ... - a(na+1)*y(n-na)
+    v}
+    for n = 1,..., [Array1.dim x].  *)
+let filter (b:vec) ?a (x:vec) =
+  let a, a_provided = match a with
+    | None -> create FFT.complex 0, false
+    | Some a -> a, true in
+  let n = max (Array1.dim a) (Array1.dim b) + Array1.dim x - 1 in
+  let y = create FFT.complex n
+  and b' = create FFT.complex n in
+  let fftb = FFT.Array1.dft FFT.Forward y b' in
+  let x' = create FFT.complex n in
+  let fftx = FFT.Array1.dft FFT.Forward y x' in
+  let y' = create FFT.complex n in
+  let iffty = FFT.Array1.dft FFT.Backward y' y in
+  copy0 b y;  FFT.exec fftb;
+  copy0 x y;  FFT.exec fftx;
+  if a_provided then (
+    let a' = create FFT.complex n in
+    let ffta = FFT.Array1.dft FFT.Forward y a' in
+    copy0 a y;  FFT.exec ffta;
+    (* FIXME: not correct: *)
+    for i = 1 to n do
+      y'.{i} <- Complex.div (Complex.mul b'.{i} x'.{i}) a'.{i}
+    done;
+  )
+  else
+    (* Consider a.{1} = 1 and a.{i} = 0 for i > 1 *)
+    for i = 1 to n do y'.{i} <- Complex.mul b'.{i} x'.{i} done;
+  FFT.exec iffty;
+  scal (1. /. float n) y;              (* normalize ifft *)
+  Array1.sub y 1 (Array1.dim x)
+;;
 
 let () =
-  let n = 10                            (* number of filters = DFT length *)
-  and fs = 1000.                        (* sampling frequency (arbitrary) *)
-  and d = 1. in                         (* duration in seconds *)
+  let cpl x = {Complex.re = x; Complex.im = 0.} in
+  let of_array a =
+    Array1.of_array FFT.complex fortran_layout (Array.map cpl a) in
+  let a = of_array [| 1.;2. |] in
+  let b = of_array [| 1.; 1.; 1. |] in
+  let x = create FFT.complex 20 in
+  for i = 1 to Array1.dim x do x.{i} <- cpl(float i) done;
+  let y  = filter ~a b x in
+  for i = 1 to Array1.dim y do
+    Printf.printf "%g " y.{i}.Complex.re
+  done;
+  Printf.printf "\n%!";
+;;
+
+let () =
+  let n = 10          (* number of filters = DFT length *)
+  and fs = 1000.      (* sampling frequency (arbitrary) *)
+  and d = 1. in       (* duration in seconds *)
+  let n_graphs = 5 in (* technically =n but not space for all graphs *)
 
   let len = truncate(ceil(fs *. d)) + 1 in (* signal duration (samples) *)
   (* sine sweep from 0 Hz to fs/2 Hz: *)
   let ch = chirp ~t1:d ~f1:(0.5 *. fs) Linear in
-
-  let t = FFT.Array1.create FFT.float fortran_layout len
-  and x = FFT.Array1.create FFT.float fortran_layout len in
-  let x' = FFT.Array1.create FFT.complex fortran_layout (len/2+1) in
-  let plan = FFT.Array1.r2c x x' in
+  let t = create FFT.float len
+  and x = create FFT.float len in
   for i = 1 to len do
     t.{i} <- float(i-1) /. fs;
     x.{i} <- ch t.{i}
   done;
-  FFT.exec plan;
+  let h = create FFT.complex n in
+  Array1.fill h Complex.one;            (* h = [| 1.; ...; 1. |] *)
 
-  let g = G.init G.X ~xsize:1000. ~ysize:300. ~nysub:2 in
+  let g = G.init G.X ~xsize:1000. ~ysize:800. ~nysub:(n_graphs + 1) in
   G.box g;
   G.pen g 1;
   G.xy g t x;
-  G.adv g;
-  G.pen g 0;
-  G.box g;
-  let y = FFT.Array1.create FFT.float fortran_layout (len/2+1) in
-  for i = 1 to Array1.dim y do y.{i} <- Complex.norm x'.{i} done;
-  G.pen g 3;
-  G.x g y;
+  let xk = create FFT.complex (Array1.dim x) in
+  let y_re = create FFT.float (Array1.dim x) in
+  for k = 1 to n_graphs do
+    G.adv g;
+    G.pen g 0;
+    G.box g;
+    (* Modulation by the complex exponential i -> exp(-I*wk*i) *)
+    let wk = 2. *. pi *. float(k-1) /. float n in
+    for i = 1 to Array1.dim x do
+      let theta = -. wk *. float i in
+      xk.{i} <- { Complex.re = x.{i} *. cos theta; im = x.{i} *. sin theta }
+    done;
+    (* Filter and display the real part *)
+    let y = filter h xk in
+    for i = 1 to Array1.dim y do y_re.{i} <- y.{i}.Complex.re done;
+    G.pen g 3;
+    G.xy g t y_re
+  done;
   G.close g
