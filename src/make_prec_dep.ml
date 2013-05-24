557e233caacc29d6f7ee2a5d2726418896c9880e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-05-24 20:03:15 +0200

    Escape '$' in regexp (to generate code).

--- src/make_prec_dep.ml
+++ src/make_prec_dep.ml
@@ -36,34 +36,34 @@ let () =
              "floatXX", "Bigarray.float64";
              "complexXX_elt", "Bigarray.complex64_elt";
              "complexXX", "Bigarray.complex64";
-             "$FFTW", "Fftw3.D"];
+             "\\$FFTW", "Fftw3.D"];
   transform "fftw3SD.ml" "fftw3S.ml"
             ["fftw_ocaml", "fftwf_ocaml"; (* C stubs *)
              "floatXX_elt", "Bigarray.float32_elt";
              "floatXX", "Bigarray.float32";
              "complexXX_elt", "Bigarray.complex32_elt";
              "complexXX", "Bigarray.complex32";
-             "$FFTW", "Fftw3.S"]
+             "\\$FFTW", "Fftw3.S"]
 
 let () =
   let for_dim = "FOR_DIM(\\([a-z0-9_]+\\), *\\([a-z0-9_]+\\))" in
   let for_hm =  "FOR_HM(\\([a-z0-9_]+\\), *\\([a-z0-9_]+\\))" in
   let debug = "DEBUG{\\([^}]+\\)};", if debug then "\\1" else "" in
   transform "fftw3_geomCF.ml" "fftw3_geomC.ml"
-            ["$LAYOUT", "c_layout";
+            ["\\$LAYOUT", "c_layout";
              "FIRST_INDEX", "0";
              "LAST_INDEX(\\([a-z0-9]+\\))", "\\1 - 1";
              for_dim, "\\1 = \\2 - 1 downto 0 "; (* do ... done *)
              for_hm,  "\\1 = 0 to \\2 - 1 ";
-             "$LT", "<";
-             "$GE", ">=";
+             "\\$LT", "<";
+             "\\$GE", ">=";
              debug];
   transform "fftw3_geomCF.ml" "fftw3_geomF.ml"
-            ["$LAYOUT", "fortran_layout";
+            ["\\$LAYOUT", "fortran_layout";
              "FIRST_INDEX", "1";
              "LAST_INDEX(\\([a-z0-9]+\\))", "\\1";
              for_dim, "\\1 = 0 to \\2 - 1 ";
              for_hm, "\\1 = \\2 - 1 downto 0 ";
-             "$LT", "<=";
-             "$GE", ">";
+             "\\$LT", "<=";
+             "\\$GE", ">";
              debug]
