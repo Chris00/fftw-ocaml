eaf036a6f2e535029fb1ad8ab2a71445178399cd
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-07-17 00:13:51 +0200

    Use the new Travis infrastructure
    http://docs.travis-ci.com/user/migrating-from-legacy/?utm_source=legacy-notice&utm_medium=banner&utm_campaign=legacy-upgrade

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,13 +1,7 @@
 # Hacking the build into Travis-CI "C" environment
 # See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
 
-DEBS="libfftw3-dev liblapack-dev"
 OPAM_PACKAGES='ocamlfind lacaml archimedes'
-
-# Install OCaml
-echo "yes" | sudo add-apt-repository ppa:avsm/ppa
-sudo apt-get update -qq
-sudo apt-get install --force-yes ocaml ocaml-native-compilers opam ${DEBS}
 
 export OPAMYES=1
 opam init
